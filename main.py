a3e03a12f10a40d18b4bb15ff5bb6b3c143e6e5e
LiBa001 <30984789+LiBa001@users.noreply.github.com>
2017-09-02 01:21:13 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,196 @@
+import discord, jPoints, random, time
+
+client = discord.Client()
+admin_id = "ADMIN-ID" # TODO: insert ADMIN-ID
+prefix = '?'
+
+num_emojis = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯']
+
+
+def leading_options(message_id):
+    option_values = []
+
+    for emoji in num_emojis:
+        option_values.append([emoji, jPoints.vote.get(message_id + emoji)])
+
+    option_values.sort(key=lambda x: x[1], reverse=True)
+
+    leadings = (option_values[0][1], [])
+    for i in range(len(option_values)):
+        if i != 0:
+            if leadings[0] == option_values[i][1]:
+                leadings[1].append(option_values[i][0])
+            else:
+                return leadings
+
+        elif i == 0:
+            leadings[1].append(option_values[i][0])
+
+    return leadings
+
+
+def time_left(message_id):
+    return int(jPoints.vote.get(message_id + "TIME")[3:]) - int(time.strftime('%M'))
+
+
+def get_leadings_str(message_id):
+    leadings_str = ""
+
+    for option in leading_options(message_id)[1]:
+        if option != leading_options(message_id)[1][0]:
+            leadings_str += ", "
+        leadings_str += option
+
+    return leadings_str
+
+
+def message_update(message_id):
+    new_text = "The voting is over in " + \
+               str(time_left(message_id)) + \
+               time.strftime(" minutes from now (%H:%M).\n") + \
+               "Currently leading: " + get_leadings_str(message_id) + \
+               " with " + str(leading_options(message_id)[0]) + " vote(s)."
+
+    return new_text
+
+
+@client.event
+async def on_ready():
+    print("Eingeloggt als:")
+    print(client.user.name)
+    print(client.user.id)
+    print("-------------")
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith(prefix + 'vote'):
+        content = message.content[6:]
+
+        commands = ['--title', '-T', '--options', '-O', '-D']
+        values = {'title': "voting / poll", '-D': 10}
+        for command in commands:
+            if content.find(command) != -1:
+                quote = content.find('"')
+                if quote != -1:
+                    content = content[quote + 1:]
+
+                quote = content.find('"')
+                if quote != -1:
+                    values[command] = content[:content.find('"')]
+                    content = content[len(values[command]) + 1:]
+
+        options = []
+        for command in values:
+            if command in ('--options', '-O'):
+                optstring = values[command]
+
+                nextopt = optstring.find(';')
+                while nextopt != -1:
+                    options.append(optstring[:nextopt].strip())
+                    optstring = optstring[nextopt + 1:]
+                    nextopt = optstring.find(';')
+
+                options.append(optstring.strip())
+
+            if command in ('--title', '-T'):
+                values['title'] = values[command]
+
+        if len(options) > 10:
+            options = options[:10]
+
+        print(options)
+
+        voting = discord.Embed(
+            title=values['title'],
+            color=0xcd3333,
+            description="Click a reaction to vote!"
+        )
+        voting.set_author(
+            name=message.author.name
+        )
+
+        # num_emojis = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯']
+
+        option_index = 0
+        for option in options:
+            voting.add_field(
+                name=num_emojis[option_index],
+                value=option,
+                inline=False
+            )
+            option_index += 1
+
+        # send message:
+
+        try:
+            await client.delete_message(message)
+        except discord.errors.Forbidden:
+            pass
+
+        duration = values['-D']
+
+        try:
+            if int(duration) >= 60:
+                duration = 59
+        except ValueError:
+            await client.send_message(message.channel, "Invalid duration.")
+            duration = 10
+
+        votemsg = await client.send_message(
+            message.channel,
+            "The voting is over in " + str(duration) + time.strftime(" minutes from now (%H:%M)."),
+            embed=voting
+        )
+
+        for emoji in num_emojis:
+            jPoints.vote.set(votemsg.id + emoji, 0)
+
+        endtime = time.strftime('%H:') + str((int(time.strftime('%M')) + int(duration)) % 60)
+        print(endtime)
+        jPoints.vote.set(votemsg.id + "TIME", endtime)
+
+        # add reactions:
+
+        for i in range(len(options)):
+            await client.add_reaction(votemsg, num_emojis[i])
+
+
+@client.event
+async def on_reaction_add(reaction, user):
+    if user.id != client.user.id:
+        if time_left(reaction.message.id) > 0:
+            jPoints.vote.add_to_Value(reaction.message.id + reaction.emoji, 1)
+            await client.edit_message(
+                reaction.message,
+                message_update(reaction.message.id)
+            )
+        elif time_left(reaction.message.id) <= 0:
+            await client.edit_message(
+                reaction.message,
+                "The voting is over.\n"
+                "Winner(s): " + get_leadings_str(reaction.message.id) +
+                " with " + leading_options(reaction.message.id)[0] + " vote(s)."
+            )
+
+
+@client.event
+async def on_reaction_remove(reaction, user):
+    if user.id != client.user.id:
+        if time_left(reaction.message.id) > 0:
+            jPoints.vote.remove_from_Value(reaction.message.id + reaction.emoji, 1)
+            await client.edit_message(
+                reaction.message,
+                message_update(reaction.message.id)
+            )
+        elif time_left(reaction.message.id) <= 0:
+            await client.edit_message(
+                reaction.message,
+                "The voting is over.\n"
+                "Winner(s): " + get_leadings_str(reaction.message.id) +
+                " with " + str(leading_options(reaction.message.id)[0]) + " vote(s)."
+            )
+
+
+if __name__ == "__main__":
+    client.run("[BOT-TOKEN]") # TODO: insert BOT-TOKEN
